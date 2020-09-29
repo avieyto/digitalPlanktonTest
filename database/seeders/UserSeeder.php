40990a9b6d72ae29725097299a1f95b94b309a80
Adrian Vieyto <avieyto@gmail.com>
2020-09-29 15:04:06 -0400

    User seeder for 100 users, password is set to 'password' for all users, this might change

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()
+            ->times(100)->state([
+                'password' => Hash::make('password')
+            ])
+            ->create();
+    }
+}
